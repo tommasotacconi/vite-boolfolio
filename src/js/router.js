bf34ceb554eb4138c5ca084c3477f41f442f2731
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-15 18:19:33 +0100

    corrected index html script with main.js and added route and export to router.js

--- src/js/router.js
+++ src/js/router.js
@@ -1,14 +1,18 @@
-import { createMemoryHistory, createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import AppHome from '../pages/AppHome.vue';
 import AppAbout from '../pages/AppAbout.vue';
+import AppProjects from '../pages/AppProjects.vue';
 
 const routes = [
   { path: '/', component: AppHome, name: 'home' },
+  { path: '/projects', component: AppProjects, name: 'projects' },
   { path: '/about', component: AppAbout, name: 'about' },
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
+
+export { router };
