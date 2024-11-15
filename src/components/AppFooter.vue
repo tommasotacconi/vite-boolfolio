ccc30a4b327bd39af112b5b213210613ff397a72
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-15 18:20:57 +0100

    added header and footercomponents

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+	data() {
+		return {
+		  
+		}
+	}
+}
+</script>
+
+<template>
+	<footer>
+		<p>Copyright &#169;	2024 Tommaso Tacconi.</p>
+	</footer>
+</template>
+
+<style scoped>
+footer {
+	height: 400px;
+	background-color: rgb(0, 45, 167);
+
+	color: #fff;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+	data() {
+		return {
+		  
+		}
+	},
+}
+</script>
+
+<template>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Portfolio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+					<router-link :to="{ name: 'home' }" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+					<router-link :to="{ name: 'projects' }" class="nav-link">Projects</router-link>
+        </li>
+        <li class="nav-item">
+					<router-link :to="{ name: 'about' }" class="nav-link">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<style scoped>
+</style>
