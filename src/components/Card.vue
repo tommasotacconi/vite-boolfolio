ecfad2b9d57936d3f130f56c1b6efca2423bf4e6
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-15 01:13:38 +0100

    created getImagePath method to manage img from api

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,6 +5,11 @@ export default {
 		  
 		}
 	},
+	methods: {
+		getImagePath: function (imgPath) {
+			return new URL(imgPath, 'http://localhost:8000/').href;
+		},
+	},
 	props: {
 		cardProject: {
 			type: Object,
@@ -17,7 +22,7 @@ export default {
 <template>
   <!-- Card Bootstrap -->
   <div class="card">
-    <img :src="cardProject.image_url" class="card-img-top" alt="card.name">
+    <img :src="getImagePath(`storage/${cardProject.img_url}`)" class="card-img-top" alt="card.name">
     <div class="card-body m-0 p-2">
       <h5 class="card-title playing-card">{{ cardProject.name }}</h5>
       <p class="playing-card-archetipe">{{ cardProject.authors }}</p>
