b9edfe27b09302d2bc4c792807a3a825f0944f63
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-11-20 09:48:51 +0100

    added card expansion at mouseenter in AppProjects.vue and related css

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,6 @@
 export default {
 	data() {
 		return {
-		  
 		}
 	},
 	methods: {
@@ -14,28 +13,70 @@ export default {
 		cardProject: {
 			type: Object,
 			required: true
-		}
+		},
+		state: {
+			type: Object,
+			required: false,
+		},
 	}
 }
 </script>
 
 <template>
   <!-- Card Bootstrap -->
-  <div class="card">
-    <img :src="getImagePath(`storage/${cardProject.img_url}`)" class="card-img-top" :alt="cardProject.name">
-    <div class="card-body m-0 p-2">
-      <h5 class="card-title playing-card">{{ cardProject.name }}</h5>
-      <p class="playing-card-archetipe"><span class="card-data">Authors</span>: {{ cardProject.authors }}</p>
-    </div>
-  </div>
+	<div class="card">
+		<Transition>
+			<img :src="getImagePath(`storage/${cardProject.img_url}`)" class="card-img-top" :alt="cardProject.name" v-if="state.show">
+		</Transition>
+		<div class="card-body m-0 p-2">
+			<h5 class="card-title playing-card">{{ cardProject.name }}</h5>
+			<Transition>
+				<p v-if="state.show" class="playing-card-archetipe"><span class="card-data">Authors</span>: {{ cardProject.authors }}</p>
+			</Transition>
+		</div>
+		<Transition>
+			<div class="button-wrapper" v-if="state.hovered">
+				<button class="btn btn-primary">Mostra</button>
+			</div>
+		</Transition>
+	</div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .card {
 	height: 220px;
+
+	position: relative;
+	overflow-y: hidden;
+
+	transition: height .5s, display 1s allow-discrete;
+}
+
+.col-2.expanded > .card {
+	height: 330px;
 }
 
 .card-data {
 	color: #5081b3;
 }
+
+button {
+	background-color: #5082b3dd;
+	border-color: transparent;
+	color: #000;
+	position: absolute;
+	left: 10px;
+	bottom: 10px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+
 </style>
--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -8,15 +8,19 @@ export default {
 		return {
 		  projectsList: [],
 			apiUrl: 'http://127.0.0.1:8000/api/projects',
+			isLoaded: false,
+			colsPerLine: 6,
+			currentHoveredDivIndex: null,
 		}
 	},
 	methods: {
-		getProjectList() {
+		getProjectsList() {
       axios.get(this.apiUrl)
         .then(response => {
           // handle success
 					console.log(response.data);
 					this.projectsList = response.data.results;
+					this.isLoaded = true;
         })
         .catch(function (error) {
           // handle error
@@ -28,25 +32,63 @@ export default {
 		Card,
 	},
 	created() {
-		this.getProjectList();
-	}
+		this.getProjectsList();
+	},
+	computed: {
+		numberOfColsGroups() {
+			return Math.ceil(this.projectsList.length / this.colsPerLine);
+		},
+		collapsedColsIndexes() {
+			let result = [];
+			for (let i = 0; i < this.numberOfColsGroups; i++) {
+				let isCurrentExpandedColGroup = false;
+				let indexesArray = [];
+				for (let j = 0; j < this.colsPerLine; j++) {
+					let colIndex = j + this.colsPerLine * i;
+					indexesArray.push(colIndex);
+					if (colIndex === this.currentHoveredDivIndex) isCurrentExpandedColGroup = true;
+				}
+				if (isCurrentExpandedColGroup) {
+					result = indexesArray;
+					break;
+				};	
+			}
+			console.log(result);
+			return result;
+		},
+		// Compute an object with keys names given by cards indexes and values of boolean type, used in card to decide whether or not all card details. Expanded card show all details in a starting card plus a button 'show'.  
+		detailsShowsList() {
+			let result = {};
+			for (let i = 0; i < this.projectsList.length; i++) {
+					let colIndex = i;
+					if (this.collapsedColsIndexes.includes(colIndex) && colIndex !== this.currentHoveredDivIndex) result[colIndex] = false;
+					else result[colIndex] = true;
+			}
+			console.log(result);
+			return result;
+		},
+	},
 }
 </script>
 
 <template>
+	<!-- Loader -->
+	<div class="loader-container" v-if="!isLoaded">
+		<div class="loader mx-auto"></div>
+	</div>
 	<!-- Main -->
-  <main class="">
+  <main class="pb-4" v-else>
 		<h1 class="text-center py-5">Projects</h1>
     <div class="card-container container-md">
       <!-- Contatori di card -->
       <div class="filter-counter-box row d-block p-4">
-        <div>Completed projects: <span class="counter-pillow"><span class="pillow-text">{{ projectsList.length }}</span></span></div> <!-- counter for projects -->
+        <div>Completed projects: <span class="badge rounded-pill text-bg-dark"><span class="pillow-text">{{ projectsList.length }}</span></span></div> <!-- counter for projects -->
       </div>
       <!-- Row di cols -->
       <div class="row g-3 mt-5">
         <!-- Cols -->
-        <div class="col-2 card-col" :class="{ 'offset-1': index % 5 === 0 }" v-for="(project, index) in projectsList" :key="index">
-          <Card :cardProject="project"	/>
+        <div class="col-2 card-col" :class="{ 'collapsed': collapsedColsIndexes.includes(index) && index !== currentHoveredDivIndex, 'expanded': currentHoveredDivIndex === index }" v-for="(project, index) in projectsList" :key="index">
+          <Card :cardProject="project"	:state="{show: detailsShowsList[index], hovered: index === currentHoveredDivIndex}" @mouseenter="currentHoveredDivIndex = index" @mouseleave="currentHoveredDivIndex = null" />
         </div>
       </div>
     </div>
@@ -54,16 +96,62 @@ export default {
 </template>
 
 <style lang="scss">
-.counter-pillow {
-    padding: 3px 10px;
-    margin-left: 5px;
-    background-color: #ffae4b;
-    border-radius: 15px;
-		
-    color: rgb(var(--bs-dark-rgb));
+.loader-container {
+	height: calc(100vh - 56px);
+}
 
-		.pillow-text {
-			vertical-align: 5%;
-		}
-  }
+// css loader, credits to CSS Loaders
+/* HTML: <div class="loader"></div> */
+.loader {
+	--loader-w: 50px;
+  width: var(--loader-w);
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 8px solid #000;
+	position: relative;
+	top: calc(50% - var(--loader-w) / 2);
+  animation:
+    l20-1 0.8s infinite linear alternate,
+    l20-2 1.6s infinite linear;
+}
+@keyframes l20-1{
+   0%    {clip-path: polygon(50% 50%,0       0,  50%   0%,  50%    0%, 50%    0%, 50%    0%, 50%    0% )}
+   12.5% {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100%   0%, 100%   0%, 100%   0% )}
+   25%   {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100% 100%, 100% 100%, 100% 100% )}
+   50%   {clip-path: polygon(50% 50%,0       0,  50%   0%,  100%   0%, 100% 100%, 50%  100%, 0%   100% )}
+   62.5% {clip-path: polygon(50% 50%,100%    0, 100%   0%,  100%   0%, 100% 100%, 50%  100%, 0%   100% )}
+   75%   {clip-path: polygon(50% 50%,100% 100%, 100% 100%,  100% 100%, 100% 100%, 50%  100%, 0%   100% )}
+   100%  {clip-path: polygon(50% 50%,50%  100%,  50% 100%,   50% 100%,  50% 100%, 50%  100%, 0%   100% )}
+}
+@keyframes l20-2{ 
+  0%    {transform:scaleY(1)  rotate(0deg)}
+  49.99%{transform:scaleY(1)  rotate(135deg)}
+  50%   {transform:scaleY(-1) rotate(0deg)}
+  100%  {transform:scaleY(-1) rotate(-135deg)}
+}
+
+.counter-pillow {
+	padding: 3px 10px;
+	margin-left: 5px;
+	background-color: #ffae4b;
+	border-radius: 15px;
+	
+	color: rgb(var(--bs-dark-rgb));
+
+	.pillow-text {
+		vertical-align: 5%;
+	}
+}
+
+.col-2 {
+	transition: width 1s;
+}
+
+.col-2.expanded {
+	width: calc(100% * 6 / 12);
+}
+
+.col-2.collapsed {
+	width: calc(100% * (6 / 12) / 5);
+}
 </style>
