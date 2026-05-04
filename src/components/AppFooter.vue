44d814ac7356728676e701ce811215cdbc3a87df
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-05-04 16:37:43 +0200

    style: Adjust indentation

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,24 +1,25 @@
 <script>
-export default {
-	data() {
-		return {
-		  
+	export default {
+		data() {
+			return {
+
+			}
 		}
 	}
-}
 </script>
 
 <template>
 	<footer>
-		<p class="p-1">Copyright &#169;	2024 Tommaso Tacconi.</p>
+		<p class="p-1">Copyright &#169; 2024 Tommaso Tacconi.</p>
 	</footer>
 </template>
 
 <style lang="scss" scoped>
-footer {
-	height: 400px;
-	background-color: rgb(0, 45, 167);
+	footer {
+		height: 400px;
+		background-color: rgb(0, 45, 167);
 
-	color: #fff;
-}
+		color: #fff;
+	}
+
 </style>
