d1f224191e5b35577209e49b961e238171c11221
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-05-07 16:35:55 +0200

    feat: Add css for cursor and syntax highlighting, import them along with bootstrap, add bounce-in animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
 // Import for router
 import { router } from './router';
+import '../style/general.scss';
+import 'bootstrap/js/dist/collapse';
 import App from '../App.vue';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
