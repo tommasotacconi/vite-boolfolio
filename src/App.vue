7bd63bdb380b495dc26aa28061c4e6a850d0e633
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-05-07 16:49:13 +0200

    feat: Set footer just outside the page (expand main to page button)

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,60 @@
 <script>
-import AppHeader from './components/AppHeader.vue';
-import AppFooter from './components/AppFooter.vue';
-import { RouterView } from 'vue-router';
+	import AppHeader from './components/AppHeader.vue';
+	import AppFooter from './components/AppFooter.vue';
+	import { RouterView } from 'vue-router';
 
-export default {
-	data() {
-		return {
+	export default {
+		data() {
+			return {
 
-		}
-	},
-	components: {
-		AppHeader,
-		AppFooter
-	},
-}
+			}
+		},
+		components: {
+			AppHeader,
+			AppFooter
+		},
+	}
 </script>
 
 <template>
-	<!-- Header -->
-	<AppHeader />
+	<!-- Immediately visible content -->
+	<div class="page">
+		<header>
+			<AppHeader id="header" />
+		</header>
+		<main>
+			<RouterView id="main-view" />
+		</main>
+	</div>
 
-	<!-- Dinamic content with Router-link	 -->
-	<RouterView id="router-view"/>
-
-	<!-- Footer -->
-	<AppFooter />
+	<!-- Not shown immediately -->
+	<AppFooter id="footer" />
 </template>
 
 <style lang="scss">
-@use 'bootstrap/scss/bootstrap.scss' as *;
+	main {
+		flex: 1;
+	}
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+	.page {
+		min-height: 100vh;
+		display: flex;
+		flex-direction: column;
+	}
+
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+		transition: filter 300ms;
+	}
+
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+
+	.logo.vue:hover {
+		filter: drop-shadow(0 0 2em #42b883aa);
+	}
 </style>
 <!-- final commit -->
