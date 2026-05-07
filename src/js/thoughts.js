7eccb7cf0b23cd27be171b6d8b5a7c5a4921aa70
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-05-07 17:12:54 +0200

    feat: Add introduction in home, with name input and live writing animation on pushing enter

---
+++ src/js/thoughts.js
@@ -0,0 +1,44 @@
+// Thoughts per time intervals
+export default {
+  '7-9': {
+    name: 'earlyMorning',
+    sentences: ['Sta iniziando la giornata.', 'Un nuovo giorno attende'],
+  },
+  '9-12': {
+    name: 'morning',
+    sentences: ['Probabilmente avrai qualche impegno imminente', 'La giornata sarà lunga e avrai delle sfide da affrontare'],
+  },
+  '12-14': {
+    name: 'midDay',
+    sentences: ['È orario di pranzo.', 'Forse starai correndo a pranzo'],
+  },
+  '14-15': {
+    name: 'firstAfternoon',
+    sentences: ['Sta iniziando il pomeriggio.', "A quest'ora un po' ci si addormenta", 'Starai organizzando il pomeriggio'],
+  },
+  '15-19': {
+    name: 'afternoon',
+    sentences: [
+      'Magari ti serve una pausa',
+      'Credo che qualche passo ti possa aiutare in questo pomeriggio.',
+      "Mangia pure se ti è venuta un po' fame.",
+    ],
+  },
+  '19-22': {
+    name: 'evening',
+    sentences: [
+      "C'è ancora tempo per inseguire un obiettivo quotidiano",
+      'Forse hai ancora qualche impegno da svolgere',
+      'Prenditi del tempo',
+      'Spero che potrai fare buon uso di ciò che hai imparato oggi, le esperienze che facciamo ci aiutano a capire chi siamo.',
+    ],
+  },
+  '22-2': {
+    name: 'earlyNight',
+    sentences: ['Spero che tu passa una buona notte.', 'Ancora poche energie ti tengono sveglio'],
+  },
+  '2-6': {
+    name: 'night',
+    sentences: ['Ora è notte', 'Sicuramente ti conviene riposare', "Che silenzio a quest'ora."],
+  },
+};
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,25 +1,154 @@
 <script>
-export default {
-	data() {
-		return {
-		  
+	import typewriter from '../js/typewriter';
+	import thoughts from '../js/thoughts';
+
+	const getRandInt = (max) => Math.floor(Math.random() * (max + 1));
+	const currHour = 3;
+	let thought = '';
+	for (const [interval, { sentences }] of Object.entries(thoughts)) {
+		const [start, end] = interval.split('-');
+		if (currHour >= start && currHour < end) {
+			thought = sentences[getRandInt(sentences.length - 1)];
+			break;
 		}
-	},
-	components: {
 	}
-}
+
+	export default {
+		data() {
+			return {
+				name: '',
+				gender: '',
+				loading: false,
+				showInput: true,
+			}
+		},
+		methods: {
+			async handleConfirm(e) {
+				if (!this.name) return;
+
+				this.loading = true;
+				this.resetGender();
+				this.gender = 'female';
+
+				try {
+					if (this.gender) return;
+					const county = 'IT';
+
+					const res = await fetch(`https://gender-api.com/get?name=${this.name}&country=${country}&key=${import.meta.env.VITE_GENDER_API_KEY}`);
+					({ gender: this.gender = '', meaning: this.meaning } = await res.json());
+				} catch (err) {
+					console.log(err);
+				} finally {
+					this.loading = false;
+					this.showInput = false;
+				}
+			},
+			resetGender() {
+				this.gender = '';
+			}
+		},
+		watch: {
+			gender(newValue) {
+				if (!newValue) return;
+
+				const concordance = this.gender.toLowerCase() === 'male' ? 'o' : 'a';
+				const isIncompleteSentence = thought.slice(-1) !== '.';
+				const textBl = `\`Ciao ${this.name}, benvenut${concordance} nel mio portfolio personale!
+
+${thought}${isIncompleteSentence ?
+						', ma qualora volessi dare un\'occhiata,'
+						: ' Comunque, aggiungo solo che'} alla pagina 'progetti' trovi i collegamenti ai siti dimostrativi più recenti che ho realizzato, mentre ad 'info' il mio percorso e i miei contatti.
+
+Ah dimenticavo! Il sito è in test, quindi se notassi qualche funzionamento inatteso, puoi farmelo sapere alla mia mail.\``;
+				const bl = this.$refs.codeBl;
+				bl.innerHTML = textBl;
+				console.log('Start typewriter...');
+				typewriter.createWithProcessing(bl);
+			}
+		}
+	}
 </script>
 
 <template>
-	<h1>Pagina di Home</h1>
+	<div class="container">
+		<h1 class="text-center py-5">Introduzione</h1>
+
+		<div class="code-wrapper">
+			<pre class="cod-ex-block">
+<code ref="codeBl" class="language-javascript typewriter"></code>
+			</pre>
+
+			<Transition name="bounce">
+				<div v-if="showInput" class="input-field">
+					<div class="form-floating">
+						<input id="name" class="form-control" v-model.trim="name" placeholder="inserisci nome" type="text"
+							@keypress.enter="handleConfirm">
+						<label for="name">Iserisci il tuo nome e avvia l'introduzione con 'invio'</label>
+					</div>
+				</div>
+			</Transition>
+		</div>
+	</div>
 </template>
 
-<style scoped>
-h1 {
-	height: 80vh;
-	text-align: center;
-	line-height: 80vh;
-	vertical-align: middle;
-	color:lightsteelblue
-}
+<style lang="scss">
+	h1 {
+		vertical-align: middle;
+		color: #8b949e
+	}
+
+	pre {
+		position: relative;
+	}
+
+	/* Utility */
+	.container {
+		padding: 0 20px;
+		margin: 0 auto;
+		max-width: 1150px;
+	}
+
+	.code-wrapper {
+		position: relative;
+	}
+
+	.bounce-enter-active {
+		animation: bounce-in 0.5s;
+	}
+
+	.bounce-leave-active {
+		animation: bounce-in 0.5s reverse;
+	}
+
+	.input-field {
+		width: 400px;
+		// outline: 3px solid white;
+		// border-radius: 5px;
+
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		translate: -50% -50%;
+		z-index: 1;
+
+		font-size: 1rem;
+
+		.form-floating>.form-control {
+			color: #a5d6ff;
+			background-color: #24292f;
+
+			&,
+			&:focus,
+			&:not(:placeholder-shown) {
+				&~label {
+					color: #8b949e;
+					background-color: transparent;
+
+					&::after {
+						background: transparent !important;
+					}
+				}
+			}
+		}
+	}
 </style>
