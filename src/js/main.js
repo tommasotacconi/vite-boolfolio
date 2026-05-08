e99dba6d80038d24642fb98c8ea6f8146a3cb6d5
tommmasotacconi <tommaso.tacconi@gmail.com>
2026-05-08 18:00:01 +0200

    chore: Install bootstrap-icons

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import { router } from './router';
 import '../style/general.scss';
 import 'bootstrap/js/dist/collapse';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from '../App.vue';
 
 createApp(App).use(router).mount('#app');
